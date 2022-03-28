07aeb56e36f418012f4a95c39fca3f92cb5e2346
maitrakesparkbiz <nirmal.maitrak@esparkbizmail.com>
2022-03-28 17:09:57 +0530

    online and offline status

--- home.php
+++ home.php
@@ -24,7 +24,7 @@ if ($result->num_rows > 0) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
+  <title>Developed by Maitrak Nirmal</title>
   <style>
   @import url("https://fonts.googleapis.com/css?family=Space+Mono&display=swap");
 
